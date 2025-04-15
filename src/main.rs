6dc0cedae91bbc4e4253b4602bc9dd578a324708
isan <m21029@g.metro-cit.ac.jp>
2025-04-15 09:52:26 +0900

    fix: Change File::open to syscall::open.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::fs::File;
 use std::io;
-use std::os::unix::io::AsRawFd;
 
 fn from_syscall_error(error: syscall::Error) -> io::Error {
     io::Error::from_raw_os_error(error.errno as i32)
@@ -10,13 +8,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let fd_path = format!("/scheme/chan/{}", "tmp/unix-domain-socket/test");
     println!("scheme path: {}", fd_path);
 
+    println!("file open");
+    let socket_fd = syscall::open(
+        "/scheme/file/home/user/test",
+        syscall::O_CREAT | syscall::O_RDWR,
+    )
+    .map_err(from_syscall_error)?;
+
     println!("open sender");
     let sender_fd =
         syscall::open(fd_path, syscall::O_CREAT | syscall::O_RDWR).map_err(from_syscall_error)?;
 
-    println!("file open");
-    let file = File::open("/home/user/test")?;
-    let socket_fd = file.as_raw_fd() as usize;
     println!("sendfd");
     let res = syscall::sendfd(sender_fd, socket_fd, 0, 0).map_err(from_syscall_error)?;
 
