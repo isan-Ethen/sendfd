b47a80ec204a30b7e313fb9f3fa910b1219afd36
isan <m21029@g.metro-cit.ac.jp>
2025-04-16 09:53:08 +0900

    wip: Implement connect_gate.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use libc::{bind, connect, socket};
+use libc::{connect, socket};
 use std::ffi::CString;
 use std::io::{self};
 use std::mem;
@@ -7,25 +7,6 @@ type Result<T> = std::result::Result<T, io::Error>;
 
 fn from_syscall_error(error: syscall::Error) -> io::Error {
     io::Error::from_raw_os_error(error.errno as i32)
-}
-
-fn str2c_char_array(s: String) -> Result<[c_char; 108], ()> {
-    if s.len() > 108 {
-        eprintln!("path is longer than 108");
-        return Err(());
-    }
-    match CString::new(s) {
-        Ok(c_string) => {
-            let bytes = c_string.as_bytes_with_nul();
-
-            let mut array = [0 as c_char; 108];
-            for (i, &byte) in bytes.iter().enumerate().take(108) {
-                array[i] = byte as c_char;
-            }
-            Ok(array)
-        }
-        Err(_) => Err(()),
-    }
 }
 
 fn connect_gate(path: &str) -> Result<RawFd> {
@@ -42,21 +23,19 @@ fn connect_gate(path: &str) -> Result<RawFd> {
     let mut gate_addr: libc::sockaddr_un = unsafe { mem::zeroed() };
     gate_addr.sun_family = libc::AF_UNIX as libc::sa_family_t;
 
-    // convert cstring to c_char array
-    unsafe {
-        let path_bytes = c_path.as_bytes_with_nul();
-        let dest_len = gate_addr.sun_path.len();
+    let path_bytes = c_path.as_bytes_with_nul();
 
-        if path_bytes.len() > gate_addr.sun_path.len() {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "path is too long",
-            ));
-        }
+    // check len of path
+    if path_bytes.len() > gate_addr.sun_path.len() {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "path is too long",
+        ));
+    }
 
-        for (i, &byte) in path_bytes.iter().enumerate() {
-            gate_addr.sun_path[i] = byte as libc::c_char;
-        }
+    // write path to gate_addr
+    for (i, &byte) in path_bytes.iter().enumerate() {
+        gate_addr.sun_path[i] = byte as libc::c_char;
     }
 
     // connect socket
@@ -74,7 +53,7 @@ fn connect_gate(path: &str) -> Result<RawFd> {
     Ok(gate)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     let fd_path = format!("chan:{}", "/tmp/unix-domain-socket/test");
     println!("scheme path: {}", fd_path);
 
